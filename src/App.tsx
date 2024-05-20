34492bfc1797d822868286a2f616b3ee18f0c623
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-05-20 11:21:59 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import Footer from "./layouts/components/Footer/Footer";
+import Header from "./layouts/components/Header/Header";
 import RouterCom from "./routers/route";
 
 function App() {
-  return (
-    <>
-      <RouterCom />
-    </>
-  );
+    return (
+        <>
+            <div className="">
+                <Header />
+            </div>
+            <div className="">
+                <RouterCom />
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
